05a4c57ac54aa1e8ea74f7fdfcfa8bb54145a4bb
NaderMakram <nadermakram98@gmail.com>
2025-05-29 15:22:27 +0300

    day 5

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,3 +1,16 @@
-<a {{ $attributes}}>
-    {{ $slot }}
-</a>
+{{-- key => default value --}}
+@props(['active' => false, 'type' => 'anchor'])
+
+@php
+    $element_tag = $type === 'button' ? 'button' : 'a';
+    $href = $attributes->get('href', '#')
+@endphp
+
+<{{$element_tag}}
+    {{$attributes}}
+    {!! $type === 'button' ? "onclick=\"window.location.href='{$href}'\"" : '' !!}
+    class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+    aria-current="{{ $active ? 'page' : 'false' }}"
+>
+    {{$slot}}
+</{{$element_tag}}>
