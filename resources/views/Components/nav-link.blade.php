635efa48bd49ca4c0f725320239c47aa00ae90c7
NaderMakram <nadermakram98@gmail.com>
2025-06-03 10:19:42 +0300

    updated day 5 homework

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,16 +1,23 @@
 {{-- key => default value --}}
 @props(['active' => false, 'type' => 'anchor'])
 
-@php
-    $element_tag = $type === 'button' ? 'button' : 'a';
-    $href = $attributes->get('href', '#')
-@endphp
 
-<{{$element_tag}}
+@if($type === 'anchor')
+<a
     {{$attributes}}
-    {!! $type === 'button' ? "onclick=\"window.location.href='{$href}'\"" : '' !!}
     class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
     aria-current="{{ $active ? 'page' : 'false' }}"
 >
     {{$slot}}
-</{{$element_tag}}>
+</a>
+
+@else
+<button
+    {{$attributes}}
+    class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+    aria-current="{{ $active ? 'page' : 'false' }}"
+>
+    {{$slot}}
+</button>
+
+@endif
