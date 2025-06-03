9afdf34760064339db447155078a5adca3a470b7
NaderMakram <nadermakram98@gmail.com>
2025-06-03 11:04:29 +0300

    day 6

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Arr;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            ['title' => 'Software Engineer', 'salary' => '$50,000', 'id' => 1],
+            ['title' => 'Data Analyst', 'salary' => '$90,000', 'id' => 2],
+            ['title' => 'Web Developer', 'salary' => '$22,000', 'id' => 3],
+        ],
+    ]);
+});
+
+
+Route::get('/job/{id}', function ($id) {
+    $jobs =  [
+        ['title' => 'Software Engineer', 'salary' => '$50,000', 'id' => 1],
+        ['title' => 'Data Analyst', 'salary' => '$90,000', 'id' => 2],
+        ['title' => 'Web Developer', 'salary' => '$22,000', 'id' => 3],
+    ];
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    // dd($job);
+    return view('job', [
+        'job' => $job,
+    ]);
 });
 
 Route::get('/contact', function () {
