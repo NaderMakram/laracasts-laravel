f19e31c417a2d26962728726f57dc226340e01e9
NaderMakram <nadermakram98@gmail.com>
2025-06-29 13:23:42 +0300

    Day 9

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,25 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            ['title' => 'Software Engineer', 'salary' => '$50,000', 'id' => 1],
-            ['title' => 'Data Analyst', 'salary' => '$90,000', 'id' => 2],
-            ['title' => 'Web Developer', 'salary' => '$22,000', 'id' => 3],
-        ];
-    }
-
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-        if (!$job) {
-            abort(404, 'Job not found');
-        }
-        return $job;
-    }
+    protected $table = 'job_listings';
+    protected $fillable = [
+        'title',
+        'salary',
+        'company',
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
 Route::get('/', function () {
+    // $jobs = Job::all();
+    // dd($jobs[0]->salary);
     return view('home');
 });
 
